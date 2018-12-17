ac7d22b65e30d03e79f9017ac1c3dc08fc2f591c
Michael Whitley <michaelwhitley@gmail.com>
2018-12-16 19:42:33 -0600

    wip

--- 6/6a.py
+++ 6/6a.py
@@ -1,22 +1,41 @@
+import string
+
 filename = "sample.txt"
 
 coors = []
+max_size = 20
 
-def distance_to_coor(c):
+def build_array():
     global coors
+    for j in range(0,max_size):
+        row = []
+        for i in range(0,max_size):
+            row.append('.')
+        coors.append(row)
 
-    for e in coors:
-        distance = abs(c[0]-e[0]) + abs(c[1]-e[1])
-        if distance > 0:
-            print(distance)
+def print_array():
+    global coors
+    for row in coors:
+        print(''.join(row))
+
+def add_coor(x,y, letter):
+     global coors
+     coors[x][y] = letter
+
+def assign_owners():
+    
+
+
+build_array()
+
 
 with open(filename) as my_file:
-        for line in my_file:
-            line = line.strip()
-            x_coor = int(line.split(",")[0].strip())
-            y_coor = int(line.split(",")[1].strip())
-            coors.append([x_coor, y_coor])
+    file_index = 0
+    for line in my_file:
+        line = line.strip()
+        x_coor = int(line.split(",")[0].strip())
+        y_coor = int(line.split(",")[1].strip())
+        add_coor(x_coor, y_coor, string.ascii_uppercase[file_index])
+        file_index += 1
 
-for c in coors:
-    distance_to_coor(c)
-    print("---")
+print_array()
